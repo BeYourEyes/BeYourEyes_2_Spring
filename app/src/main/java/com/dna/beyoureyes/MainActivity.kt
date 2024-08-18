632fc3b3da30736c05aac2b96618f2fc8e07b472
moon0900 <moon09@duksung.ac.kr>
2024-08-18 22:48:45 +0900

    OpenCV 모듈을 lint 검사 제외 대상으로 지정

--- app/src/main/java/com/dna/beyoureyes/MainActivity.kt
+++ app/src/main/java/com/dna/beyoureyes/MainActivity.kt
@@ -1,8 +1,13 @@
 package com.dna.beyoureyes
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import com.dna.beyoureyes.databinding.ActivityMainBinding
+import org.opencv.android.OpenCVLoader
+import org.opencv.core.CvType
+import org.opencv.core.Mat
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +18,24 @@ class MainActivity : AppCompatActivity() {
 
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        // OpenCV 초기화
+        if (!OpenCVLoader.initDebug()) {
+            Log.e("CV2TEST", "OpenCV initialization failed.")
+        } else {
+            Log.d("CV2TEST", "OpenCV initialization succeeded.")
 
+            // OpenCV 기능 테스트
+            testOpenCVFunction()
+        }
+    }
+
+    private fun testOpenCVFunction() {
+        // 3x3 크기의 CV_8UC1 타입의 Mat 객체 생성
+        val mat = Mat(3, 3, CvType.CV_8UC1)
+        Log.d("CV2TEST", "Created Mat object: Rows=${mat.rows()}, Cols=${mat.cols()}, Type=${mat.type()}")
+
+        // Mat 객체를 이용한 기본적인 작업 예제
+        mat.put(0, 0, byteArrayOf(1, 2, 3))
+        Log.d("CV2TEST", "Mat values: ${mat.get(0, 0).contentToString()}")
     }
 }
