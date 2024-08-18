95bf30d85ad83b031651075a40b24d1f9898de9f
moon0900 <moon09@duksung.ac.kr>
2024-08-18 22:48:45 +0900

    OpenCV 모듈을 lint 검사 제외 대상으로 지정

--- opencv2/sdk/build/intermediates/cxx/Debug/421b111g/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\moon0\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "D:\\Projects\\Be_Your_Eyes\\Android\\Beyoureyes_App\\opencv2\\sdk\\.cxx\\Debug\\421b111g\\arm64-v8a" ^
-  opencv_jni_shared
