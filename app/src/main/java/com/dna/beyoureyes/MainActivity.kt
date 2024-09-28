642794cdd44897e9cb7be47b64259c790a86f030
moon0900 <moon09@duksung.ac.kr>
2024-09-28 22:03:12 +0900

    맞춤 설계 STEP 1~3 레이아웃 작성

--- app/src/main/java/com/dna/beyoureyes/MainActivity.kt
+++ app/src/main/java/com/dna/beyoureyes/MainActivity.kt
@@ -5,24 +5,20 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.dna.beyoureyes.databinding.ActivityMainBinding
-import com.dna.beyoureyes.databinding.AssignBirthBinding
-import com.dna.beyoureyes.databinding.AssignGenderBinding
-import com.dna.beyoureyes.databinding.AssignNameBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    //private lateinit var binding: ActivityMainBinding
-    private lateinit var binding: AssignBirthBinding
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
 
         // 레이아웃 바인딩
-        binding = AssignBirthBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        /*
         // 하단 네비게이션 뷰 초기화
         val navView: BottomNavigationView = binding.navView
 
@@ -30,8 +26,6 @@ class MainActivity : AppCompatActivity() {
         // -> 네비게이션 파일에 설정한대로 처리할 수 있게
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
         navView.setupWithNavController(navController)
-
-         */
 
     }
 
