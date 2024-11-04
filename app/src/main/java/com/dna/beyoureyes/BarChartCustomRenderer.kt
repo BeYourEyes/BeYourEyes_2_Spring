07ba6142694aaef8cd24375da5da8203f098a4e9
Jordan <leesoyoung0112@gmail.com>
2024-11-04 16:36:15 +0900

    feat(MainActivity): 칩 버튼 클릭 시 테두리 변경 구현

---
+++ app/src/main/java/com/dna/beyoureyes/BarChartCustomRenderer.kt
@@ -0,0 +1,4 @@
+package com.dna.beyoureyes
+
+class BarChartCustomRenderer {
+}
--- app/src/main/java/com/dna/beyoureyes/MainActivity.kt
+++ app/src/main/java/com/dna/beyoureyes/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.dna.beyoureyes
 
 import android.content.ContentValues.TAG
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.TypedValue
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.dna.beyoureyes.databinding.ActivityMainBinding
@@ -31,6 +33,17 @@ class MainActivity : AppCompatActivity() {
         }
         Log.d("Chip", "isStarted")
         //startActivity(Intent(this, ResultEatActivity::class.java))
+        binding.chipTest.setOnCheckedChangeListener{ _, isChanged ->
+            if(isChanged) {
+                binding.chipTest.chipStrokeWidth = 2f.toDp(this)
+                val displayMetrics = resources.displayMetrics
+                val densityDpi = displayMetrics.densityDpi
+                println("Current screen density (DPI): $densityDpi")
+            } else {
+                binding.chipTest.chipStrokeWidth = 0f
+            }
+        }
+
 
     }
 
@@ -42,5 +55,9 @@ class MainActivity : AppCompatActivity() {
         // Mat 객체를 이용한 기본적인 작업 예제
         mat.put(0, 0, byteArrayOf(1, 2, 3))
         Log.d("CV2TEST", "Mat values: ${mat.get(0, 0).contentToString()}")
+    }
+
+    private fun Float.toDp(context: Context): Float {
+        return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, this, context.resources.displayMetrics)
     }
 }
