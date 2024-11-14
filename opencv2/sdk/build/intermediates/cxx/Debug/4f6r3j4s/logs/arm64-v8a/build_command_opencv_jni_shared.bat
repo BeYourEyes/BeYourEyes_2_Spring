a0097440a0c5055a137f3450a696525b96a00366
Jordan <leesoyoung0112@gmail.com>
2024-11-14 16:22:03 +0900

    merge conflict solve

--- opencv2/sdk/build/intermediates/cxx/Debug/4f6r3j4s/logs/arm64-v8a/build_command_opencv_jni_shared.bat
+++ opencv2/sdk/build/intermediates/cxx/Debug/4f6r3j4s/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -1,6 +1,5 @@
 @echo off
-
-"C:\\Users\\sojin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+"C:\\Users\\leeso\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -C ^
-  "C:\\Users\\sojin\\AndroidStudioProjects\\1113\\BeYourEyes_2\\opencv2\\sdk\\.cxx\\Debug\\4f6r3j4s\\arm64-v8a" ^
+  "C:\\Users\\leeso\\Desktop\\BeYourEyes_2\\opencv2\\sdk\\.cxx\\Debug\\4f6r3j4s\\arm64-v8a" ^
   opencv_jni_shared
