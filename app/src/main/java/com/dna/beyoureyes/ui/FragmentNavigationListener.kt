a8d4729ec99f1bdd914c05f8b97be5a9cab1f161
Jordan <leesoyoung0112@gmail.com>
2024-11-21 20:19:17 +0900

    fix : UserInfo 버그 수정

--- app/src/main/java/com/dna/beyoureyes/ui/FragmentNavigationListener.kt
+++ app/src/main/java/com/dna/beyoureyes/ui/FragmentNavigationListener.kt
@@ -10,11 +10,13 @@ interface FragmentNavigationListener {
     fun onNameInputRecieved(name: String)
 
     fun onGenderInputRecieved(gender : Int)
-
+    /*
     fun onBirthInputRecieved(birth : Int)
 
     fun onDiseaseInputRecieved(userDiseaseList : ArrayList<String>)
 
     fun onAllergyInputRecieved(userAllergyList : ArrayList<String>)
+
+     */
 
 }
