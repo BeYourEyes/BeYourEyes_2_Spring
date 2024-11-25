f7da4f999b35a0d9a4836359aa345aecf9b212ff
moon0900 <moon09@duksung.ac.kr>
2024-11-26 03:04:10 +0900

    Home 프래그먼트와 Firebase 데이터 연결 & .gitignore 재설정

--- app/src/main/java/com/dna/beyoureyes/model/FoodHistory.kt
+++
@@ -1,5 +0,0 @@
-package com.dna.beyoureyes.model
-
-class FoodHistory(val label: String, val kcal: Int, val img:Int) {
-
-}
--- opencv2/sdk/build/intermediates/cxx/Debug/4f6r3j4s/logs/x86/build_command_opencv_jni_shared.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-"C:\\Users\\sojin\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
-  -C ^
-  "C:\\Users\\sojin\\AndroidStudioProjects\\0825\\BeYourEyes_2\\opencv2\\sdk\\.cxx\\Debug\\4f6r3j4s\\x86" ^
-  opencv_jni_shared
