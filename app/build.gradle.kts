fc5c1697aa2e97afa8b7745b1a58b5a4f1c93223
moon0900 <moon09@duksung.ac.kr>
2025-03-20 17:05:35 +0900

    chore: gitignore 및 라이브러리 dependencies 업데이트
    - 일부 라이브러리 중복 implementation 제거
    - Firebase 라이브러리 중 불필요한 것 제거
    - gitignore 제외 경로 추가:
        .idea/
        gradle.properties

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -65,12 +65,21 @@ android {
 }
 
 dependencies {
+    // MVVM pattern
+    implementation ("androidx.activity:activity-ktx:1.7.0")
     implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.4")
     implementation ("androidx.lifecycle:lifecycle-livedata-ktx:2.8.4")
     implementation("androidx.core:core-ktx:1.13.1")
+
+    // 코루틴
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+    // UI
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+
+    // 테스트
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
@@ -81,25 +90,16 @@ dependencies {
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
 
     // Google Firebase
-    // 익명 계정을 위한 dependency 추가
     // BoM for the Firebase platform
-    implementation(platform("com.google.firebase:firebase-bom:33.6.0"))
+    implementation(platform("com.google.firebase:firebase-bom:33.6.0")) // Firebase 버전 관리
     implementation("com.google.firebase:firebase-installations:17.1.4") // FID 사용
-    implementation("com.google.firebase:firebase-analytics")
-    implementation("com.google.firebase:firebase-firestore-ktx")
-    implementation("com.google.firebase:firebase-auth-ktx")
-    implementation("com.google.firebase:firebase-storage-ktx")
-    implementation("com.google.firebase:firebase-database-ktx")
+    implementation("com.google.firebase:firebase-storage-ktx") // 이미지 저장용 Storage 사용
 
     // Glide (이미지 로드 라이브러리)
     implementation("com.github.bumptech.glide:glide:4.16.0")
 
-    // google login
-    implementation ("com.google.android.gms:play-services-auth:21.2.0")
-
     //MPAndroidChart
     implementation("com.github.PhilJay:MpAndroidChart:v3.1.0")
-
 
     //LocalDate 사용을 위한 백포팅
     implementation("com.jakewharton.threetenabp:threetenabp:1.3.0")
@@ -107,15 +107,15 @@ dependencies {
     //Review
     implementation("com.google.android.play:review:2.0.2")
 
-
     //openCV
     implementation(project(":opencv2"))
 
+    // ML Kit for Firebase
     implementation ("com.google.android.gms:play-services-mlkit-text-recognition-common:19.1.0")
     implementation ("com.google.android.material:material:1.12.0")
     implementation ("com.google.android.gms:play-services-mlkit-text-recognition-korean:16.0.1")
 
-
+    // CameraX
     implementation ("androidx.camera:camera-core:1.4.0-rc01")
     implementation ("androidx.camera:camera-camera2:1.4.0-rc01")
     implementation ("androidx.camera:camera-lifecycle:1.4.0-rc01")
@@ -123,34 +123,24 @@ dependencies {
     implementation ("androidx.camera:camera-view:1.4.0-rc01")
     implementation ("androidx.camera:camera-extensions:1.4.0-rc01")
 
-
-    //
+    // ViewPager
     implementation("androidx.viewpager2:viewpager2:1.0.0")
     implementation("com.tbuonomo:dotsindicator:5.0")
 
-    //Circle Image
+    // Circle Image
     implementation("de.hdodenhof:circleimageview:3.1.0")
 
-    //View Animations
+    // View Animations
     implementation("com.daimajia.androidanimations:library:2.4@aar")
-    // Fragment
-    implementation("androidx.fragment:fragment-ktx:1.8.2")
-    // MVVM pattern
-    implementation ("androidx.core:core-ktx:1.7.0")
-    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.5.1")
-    implementation ("androidx.activity:activity-ktx:1.7.0")
-
     implementation("com.airbnb.android:lottie:6.6.0")
 
+    // Retrofit
     implementation ("com.squareup.retrofit2:retrofit:2.9.0") // Retrofit
     implementation ("com.squareup.retrofit2:converter-gson:2.9.0") // Converter ( JSON -> 객체 매핑 )
 
     // OkHttp (HTTP 클라이언트)
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("com.squareup.okhttp3:logging-interceptor:4.11.0")
-
-    // 코루틴
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 
     // Data Store(액세스 토큰 저장용)
     implementation("androidx.datastore:datastore-preferences:1.0.0")
